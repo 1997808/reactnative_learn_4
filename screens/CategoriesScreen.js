b3f2721fae2e2d476e596e16eb9dc75ab3a98f19
1997808 <khanhhuyle22@gmail.com>
2020-03-14 14:28:09 +0700

    fake data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native'
+
+import { CATEGORIES } from '../data/dummy-data'
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  )
+}
 
 const CategoriesScreen = props => {
   // console.log(props)
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button title="Go to Meal" onPress={() => {
-        props.navigation.navigate({ routeName: 'CategoryMeals' })
-        // props.navigation.push('CategoryMeals') for the same screen with different data
-        // props.navigation.replace('CategoryMeals') for login 
-      }} />
-    </View>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+    // <View style={styles.screen}>
+    //   <Text>The Categories Screen</Text>
+    //   <Button title="Go to Meal" onPress={() => {
+    //     props.navigation.navigate({ routeName: 'CategoryMeals' })
+    //     // props.navigation.push('CategoryMeals') for the same screen with different data
+    //     // props.navigation.replace('CategoryMeals') for login 
+    //   }} />
+    // </View>
   )
 }
 
@@ -20,6 +31,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 })
 
